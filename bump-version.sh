54fe40d9c18bc015a020ed907bca3d02aac58738
Nesz <filip.krasinski@outlook.com>
2022-03-24 22:38:33 +0100

    [S20-90] [minor] add build date to version

--- bump-version.sh
+++ bump-version.sh
@@ -3,8 +3,9 @@
 
 oldVersion=$(jq -r '.["version"]' frontend/package.json)
 export OLD_VERSION=$oldVersion
-
-IFS='.' read -r -a parts <<< "$oldVersion"
+date=$(date +'%Y%m%d%H%M%S')
+IFS='-' read -r -a versionSplitByDash <<< "$oldVersion"
+IFS='.' read -r -a parts <<< "${versionSplitByDash[0]}"
 
 if [[ $(git log -1 --pretty=format:"%s") =~ .*"major".* ]]; then
     (( parts[0]++; parts[1] = 0; parts[2] = 0 ))
@@ -16,8 +17,6 @@ fi
 
 
 commit=$(git rev-parse --short HEAD)
-newVersion="${parts[0]}.${parts[1]}.${parts[2]}-$commit"
+newVersion="${parts[0]}.${parts[1]}.${parts[2]}-$date-$commit"
 export NEW_VERSION="${parts[0]}.${parts[1]}.${parts[2]}-$commit"
 perl -p -i -e 's/$ENV{OLD_VERSION}/$ENV{NEW_VERSION}/' frontend/package.json
-echo "$oldVersion"
-echo "$newVersion"
