190c89e1ac1d25bf33330f2bcd4616f717420a6d
Nesz <filip.krasinski@outlook.com>
2022-03-24 22:54:49 +0100

    [S20-90] [minor] default versioning on dev, more specified versioning in build

--- bump-version.sh
+++ bump-version.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+if [ "$1" == "git" ]; then
+    oldVersion=$(jq -r '.["version"]' frontend/package.json)
+    export OLD_VERSION=$oldVersion
+    IFS='.' read -r -a parts <<< "$oldVersion"
 
-oldVersion=$(jq -r '.["version"]' frontend/package.json)
-export OLD_VERSION=$oldVersion
-date=$(date +'%Y%m%d%H%M%S')
-IFS='-' read -r -a versionSplitByDash <<< "$oldVersion"
-IFS='.' read -r -a parts <<< "${versionSplitByDash[0]}"
+    if [[ $(git log -1 --pretty=format:"%s") =~ .*"major".* ]]; then
+        (( parts[0]++; parts[1] = 0; parts[2] = 0 ))
+    elif [[ $(git log -1 --pretty=format:"%s") =~ .*"minor".* ]]; then
+        (( parts[1]++; parts[2] = 0 ))
+    else
+        (( parts[2]++ ))
+    fi
 
-if [[ $(git log -1 --pretty=format:"%s") =~ .*"major".* ]]; then
-    (( parts[0]++; parts[1] = 0; parts[2] = 0 ))
-elif [[ $(git log -1 --pretty=format:"%s") =~ .*"minor".* ]]; then
-    (( parts[1]++; parts[2] = 0 ))
-else
-    (( parts[2]++ ))
+    export NEW_VERSION="${parts[0]}.${parts[1]}.${parts[2]}"
+    perl -p -i -e 's/$ENV{OLD_VERSION}/$ENV{NEW_VERSION}/' frontend/package.json
+
+elif [ "$1" == "build" ]; then
+    oldVersion=$(jq -r '.["version"]' frontend/package.json)
+    commit=$(git rev-parse --short HEAD)
+    date=$(date +'%Y%m%d%H%M%S')
+    export OLD_VERSION=$oldVersion
+    export NEW_VERSION="$oldVersion-$date-$commit"
+    perl -p -i -e 's/$ENV{OLD_VERSION}/$ENV{NEW_VERSION}/' frontend/package.json
 fi
-
-
-commit=$(git rev-parse --short HEAD)
-newVersion="${parts[0]}.${parts[1]}.${parts[2]}-$date-$commit"
-export NEW_VERSION="${parts[0]}.${parts[1]}.${parts[2]}-$commit"
-perl -p -i -e 's/$ENV{OLD_VERSION}/$ENV{NEW_VERSION}/' frontend/package.json
