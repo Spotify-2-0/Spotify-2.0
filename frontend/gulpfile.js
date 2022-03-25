2f4b26c153685e66613f6d67a36e4e53ad6fc756
Nesz <filip.krasinski@outlook.com>
2022-03-25 18:17:35 +0100

    [S20-90] [minor] fix gulp

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
-var gzip = require('gulp-gzip');
+var uglify = require('gulp-uglify');
 
-gulp.task('compress', function() {
-  return gulp.src(['./dist/**/*.*'])
-      .pipe(gzip())
-      .pipe(gulp.dest('./dist'));
+gulp.task('uglify', function() {
+  return gulp.src(['./dist/**/*.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
 });
