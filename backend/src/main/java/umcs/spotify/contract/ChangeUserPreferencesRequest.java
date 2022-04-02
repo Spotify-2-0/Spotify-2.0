27fbaa15307c196b20c78cc0e574035e799513cb
Adiras <wkacper@o2.pl>
2022-04-02 14:50:14 +0200

    Add display name field when changing user preferences

--- backend/src/main/java/umcs/spotify/contract/ChangeUserPreferencesRequest.java
+++ backend/src/main/java/umcs/spotify/contract/ChangeUserPreferencesRequest.java
@@ -12,7 +12,11 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class ChangeUserPreferencesRequest {
-    
+
+    @Size(min = 3, max = 16,
+            message = "Display name length must be between 3 and 16 characters")
+    String displayName;
+
     @Size(min = 1, max = 16,
             message = "First name must be between 1 and 16 characters")
     String firstName;
