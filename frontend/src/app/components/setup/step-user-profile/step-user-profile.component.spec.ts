f24e75095e711ef45ebcd8a2e3ff8126cd1e3fc6
Nesz <filip.krasinski@outlook.com>
2022-04-02 09:28:10 +0200

    [S20-104] [minor] fix tests

--- frontend/src/app/components/setup/step-user-profile/step-user-profile.component.spec.ts
+++ frontend/src/app/components/setup/step-user-profile/step-user-profile.component.spec.ts
@@ -2,16 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { StepUserProfileComponent } from './step-user-profile.component';
+import { HttpClientModule } from "@angular/common/http";
+import { UserService } from "../../../services/user.service";
 
 describe('StepUserProfileComponent', () => {
   let component: StepUserProfileComponent;
+  let userService: jasmine.SpyObj<UserService>;
   let fixture: ComponentFixture<StepUserProfileComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HttpClientModule
       ],
       declarations: [ StepUserProfileComponent ]
     })
@@ -19,6 +23,16 @@ describe('StepUserProfileComponent', () => {
   });
 
   beforeEach(() => {
+    userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+    userService['_userSubject'].next({
+      firstName: '',
+      lastName: '',
+      email: '',
+      emailConfirmed: true,
+      displayName: '',
+      id: 0
+    })
+
     fixture = TestBed.createComponent(StepUserProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
