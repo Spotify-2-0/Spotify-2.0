c77b48c9e6fddb78dd1e7296d707e6ef7fa73a2e
p-wojt <patryk.wojtiuk9@gmail.com>
2022-04-18 10:19:10 +0200

    [S20-105] [minor] navbar for logged user & add theme functionality

--- frontend/src/app/sites/logged-page/logged-page.component.ts
+++ frontend/src/app/sites/logged-page/logged-page.component.ts
@@ -1,10 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/services/theme.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-logged-page',
   templateUrl: './logged-page.component.html',
 })
-export class LoggedPageComponent {
+export class LoggedPageComponent implements OnInit {
+  avatarBlob!: string;
+  firstName!: string;
+  lastName!: string
 
-  constructor() { }
+  constructor(
+    private readonly themeService: ThemeService,
+    private readonly userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService
+      .me()
+      .subscribe(user => {
+        this.avatarBlob = this.userService.getUserProfileUrl(user.id)
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+      });
+  }
+
+  public toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
 }
